5bb398fbafb02eea520c1b8f6f97ce54d56158c7
Mariya Metelitsa <mariya.metelitsa@gmail.com>
2015-08-16 23:42:47 +0300

    modifyed logic

--- career/views.py
+++ career/views.py
@@ -31,25 +31,27 @@ def home(request):
             if form1.is_valid():
                 cd = form.cleaned_data
                 cd1 = form1.cleaned_data
+
                 link_name = str(cd['link'])
+                token_skills = [str(sk.name) for sk in cd1['Skills']]
                 crawler = Crawler()
+                error = []
+                if 'profile/view' in link_name:
+                    crawler.login()
 
-                error = ''
-                try:
-                    if 'profile/view' in link_name:
-                        crawler.login()
 
-                    token_skills = [str(sk.name) for sk in cd1['Skills']]
-                    list1 = sort_skills(get_profession(crawler.get_skills(link_name)+token_skills))[:3]
+                if not link_name and not token_skills:
+                    error.append('Please, enter some information')
+                elif not link_name and token_skills:
+                    list1 = sort_skills(get_profession(token_skills))[:3]
                     prof_obj = [Profession.objects.filter(name__icontains=item) for item in list1]
                     list_sphere = [it[0].sphere.all() for it in prof_obj]
                     spheres = []
                     for l in list_sphere:
                         spheres += [li for li in l]
-
-                    list(set(spheres))    #list of spheres
+                    list(set(spheres))
                     spheres_name = [str(s.name) for s in list(set(spheres))]
-                    spheres_descr = [s.discription for s in list(set(spheres))] #a problem here
+                    spheres_descr = [s.discription for s in list(set(spheres))]
 
                     links = [str(Course.objects.filter(sphere = sph)[0].name) for sph in list(set(spheres))]
                     link = "https://www.coursera.org/courses?categories="
@@ -59,25 +61,41 @@ def home(request):
                     dict = {}
                     for a in range(len(spheres_name)):
                         dict[spheres_name[a]] = spheres_descr[a]
+                elif link_name:
+                    try:
+                        list1 = sort_skills(get_profession(crawler.get_skills(link_name)+token_skills))[:3]
+                        prof_obj = [Profession.objects.filter(name__icontains=item) for item in list1]
+                        list_sphere = [it[0].sphere.all() for it in prof_obj]
+                        spheres = []
+                        for l in list_sphere:
+                            spheres += [li for li in l]
+                        list(set(spheres))
+                        spheres_name = [str(s.name) for s in list(set(spheres))]
+                        spheres_descr = [s.discription for s in list(set(spheres))]
+
+                        links = [str(Course.objects.filter(sphere = sph)[0].name) for sph in list(set(spheres))]
+                        link = "https://www.coursera.org/courses?categories="
+                        for b in links:
+                            link += b +','
+                        link = link[:-1]
+                        dict = {}
+                        for a in range(len(spheres_name)):
+                            dict[spheres_name[a]] = spheres_descr[a]
+                    except ValueError:
+                        error.append("Your profile does not give proper amount of information, please enter some skills")
 
 
-
-                except ValueError:
-                    error = "We have not enough information, please enter some skills"
-
-                except:
-                    if not link_name and not token_skills:
-                        error = "Please, enter some information"
+                    except:
+                        error.append("Please, check if the link is valid. You can leave it blank and just enter your skills")
 
 
-                if not error:
+                if error == []:
                     t = get_template('career/result.html')
                     result = t.render(Context({'user':request.user, 'dict': dict, 'link':link}))
                     return HttpResponse(result)
                 else:
                     form = LinkForm()
                     return render(request, 'career/home.html', {'form': form, 'error': error, 'form1': TokenForm})
-
     else:
         form = LinkForm()
     return render(request, 'career/home.html', {'form': form, 'form1': TokenForm})
