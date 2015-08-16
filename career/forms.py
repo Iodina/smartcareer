bf86f7e9191a79be6c8ef40e8f79c459ffaf0816
Mariya Metelitsa <mariya.metelitsa@gmail.com>
2015-08-16 15:38:43 +0300

    Templates

--- career/forms.py
+++ career/forms.py
@@ -3,7 +3,7 @@ from djtokeninput import TokenField
 from career.models import Skill
 
 class LinkForm(forms.Form):
-    link = forms.URLField(label='LinkedIn', max_length=200, required = True, widget=forms.TextInput(attrs={'class':'special', 'size': '40'}))
+    link = forms.URLField(label='LinkedIn', max_length=200, widget=forms.TextInput(attrs={'class':'special', 'size': '40'}), required=False)
 
 class TokenForm(forms.Form):
     Skills = TokenField(Skill, required=False)
--- career/views.py
+++ career/views.py
@@ -25,58 +25,61 @@ from algorithm1 import *
 def home(request):
     if request.method == 'POST':
         form = LinkForm(request.POST)
+        form1 = TokenForm(request.POST)
+
         if form.is_valid():
-            cd = form.cleaned_data
-            link_name = str(cd['link'])
-            crawler = Crawler()
+            if form1.is_valid():
+                cd = form.cleaned_data
+                cd1 = form1.cleaned_data
+                link_name = str(cd['link'])
+                crawler = Crawler()
 
-            error = ''
-            try:
-                if "linkedin" in link_name:
-                    if 'profile/view' in link_name:
-                        # link_name = crawler.get_local_link(link_name)
-                        crawler.login()
-                # profs = get_profession(crawler.get_skills(link_name))
-                # list1 = sort_skills(profs)[:3]
-                list1 = sort_skills(get_profession(crawler.get_skills(link_name)))[:3]
+                error = ''
+                try:
+                    if "linkedin" in link_name:
+                        if 'profile/view' in link_name:
+                            crawler.login()
+
+                    token_skills = [str(sk.name) for sk in cd1['Skills']]
+                    list1 = sort_skills(get_profession(crawler.get_skills(link_name)+token_skills))[:3]
+                    prof_obj = [Profession.objects.filter(name__icontains=item) for item in list1]
+                    list_sphere = [it[0].sphere.all() for it in prof_obj]
+                    spheres = []
+                    for l in list_sphere:
+                        spheres += [li for li in l]
+
+                    list(set(spheres))    #list of spheres
+                    spheres_name = [str(s.name) for s in list(set(spheres))]
+                    spheres_descr = [s.discription for s in list(set(spheres))] #a problem here
+
+                    links = [str(Course.objects.filter(sphere = sph)[0].name) for sph in list(set(spheres))]
+                    link = "https://www.coursera.org/courses?categories="
+                    for b in links:
+                        link += b +','
+                    link = link[:-1]
+                    dict = {}
+                    for a in range(len(spheres_name)):
+                        dict[spheres_name[a]] = spheres_descr[a]
 
 
-                prof_obj = [Profession.objects.filter(name__icontains=item) for item in list1]
-                list_sphere = [it[0].sphere.all() for it in prof_obj]
-                spheres = []
-                for l in list_sphere:
-                    spheres += [li for li in l]
-
-                list(set(spheres))    #list of spheres
-                spheres_name = [str(s.name) for s in list(set(spheres))]
-                spheres_descr = [s.discription for s in list(set(spheres))] #a problem here
-
-                links = [str(Course.objects.filter(sphere = sph)[0].name) for sph in list(set(spheres))]
-                link = "https://www.coursera.org/courses?categories="
-                for b in links:
-                    link += b +','
-                link = link[:-1]
-                dict = {}
-                for a in range(len(spheres_name)):
-                    dict[spheres_name[a]] = spheres_descr[a]
+                except:
+                    if not link_name and not token_skills:
+                        error = "Please, enter some information"
+                    else:
+                        error = "We have not enough information, please enter some skills"
 
 
-            except:
-                if "linkedin" not in link_name:
-                    error = "Please enter link from LinkedIn"
+                if not error:
+                    t = get_template('career/result.html')
+                    result = t.render(Context({'user':request.user, 'dict': dict, 'link':link}))
+                    return HttpResponse(result)
                 else:
-                    error = "Please fill your profile with more information or enter your skills in the field below"
-            if not error:
-                t = get_template('career/result.html')
-                result = t.render(Context({'user':request.user, 'dict': dict, 'link':link}))
-                return HttpResponse(result)
-            else:
-                form = LinkForm()
-                return render(request, 'career/home.html', {'form': form, 'error': error, 'form1': TokenForm})
+                    form = LinkForm()
+                    return render(request, 'career/home.html', {'form': form, 'error': error, 'form1': TokenForm})
 
     else:
         form = LinkForm()
-    return render(request, 'career/home.html', {'form': form})
+    return render(request, 'career/home.html', {'form': form, 'form1': TokenForm})
 
 
 #
